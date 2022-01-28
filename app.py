37024e065809cca092bace0ca99c36559eaebe50
Daniel Monaghan <mowglixx@users.noreply.github.com>
2022-01-28 14:37:24 +0000

    Update app.py

--- app.py
+++ app.py
@@ -1,30 +1,47 @@
-from flask import Flask, render_template, url_for
+from flask import Flask, redirect, render_template, request
 from flask_sqlalchemy import SQLAlchemy
 from datetime import datetime
 
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///database.db'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = 'False'
 db = SQLAlchemy(app)
 
 
-class Todo(db.Model):
+class User(db.Model):
     id = db.Column(db.Integer, primary_key=True)
-    content = db.Column(db.String(200), primary_key=True)
-    completed = db.Column(db.Integer, default=0)
+    username = db.Column(db.String(20), primary_key=True)
+    display_name = db.Column(db.String(20), primary_key=True)
+    password = db.Column(db.String(200), primary_key=True)
+    password = db.Column(db.String(200), primary_key=True)
     date_created = db.Column(db.DateTime, default=datetime.utcnow)
 
     def __repr__(self):
         return '<Task %r>' % self.id
 
 
-@app.route('/', methods=['POST', 'GET'])
+# def :
+
+
+@app.route('/')
 def index():
-    return render_template('index.html')
+    return render_template('home.html', title="Home")
 
 
-@app.route('/<name>')
-def name(name):
-    return render_template('name.html', name=name)
+@app.route('/messagehelper/<string:title>')
+@app.route('/messagehelper/<string:title>/<string:content>')
+def message(title, content='On this page you can add your own title by typing in the addressbar after the <code>/title/&ltHere & gt</code>'):
+    # decent example of GET use
+    return render_template('messagehelper.html', title=title, content=content)
+
+
+@app.route('/login', methods=['POST', 'GET'])
+def login():
+    if request.method == 'POST':
+        print('Magic')
+        return redirect('/messagehelper/Logged In/You have successfully logged in.')
+    else:
+        return render_template('login.html', title="Login")
 
 
 if __name__ == "__main__":
