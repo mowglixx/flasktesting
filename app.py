65047b450ebdc6152e9700064854449f1e660b6c
Daniel Monaghan <mowglixx@users.noreply.github.com>
2022-04-13 02:40:31 +0100

    too much

--- app.py
+++ app.py
@@ -1,23 +1,23 @@
 from flask import Flask, redirect, render_template, request
-from flask_sqlalchemy import SQLAlchemy
+# from flask_sqlalchemy import SQLAlchemy
 from datetime import datetime
 
 app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///database.db'
-app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = 'False'
-db = SQLAlchemy(app)
+# app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///database.db'
+# app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = 'False'
+# db = SQLAlchemy(app)
 
 
-class User(db.Model):
-    id = db.Column(db.Integer, primary_key=True)
-    username = db.Column(db.String(20), primary_key=True)
-    display_name = db.Column(db.String(20), primary_key=True)
-    password = db.Column(db.String(200), primary_key=True)
-    password = db.Column(db.String(200), primary_key=True)
-    date_created = db.Column(db.DateTime, default=datetime.utcnow)
+# class User(db.Model):
+#     id = db.Column(db.Integer, primary_key=True)
+#     username = db.Column(db.String(20), primary_key=True)
+#     display_name = db.Column(db.String(20), primary_key=True)
+#     password = db.Column(db.String(200), primary_key=True)
+#     password = db.Column(db.String(200), primary_key=True)
+#     date_created = db.Column(db.DateTime, default=datetime.utcnow)
 
-    def __repr__(self):
-        return '<Task %r>' % self.id
+#     def __repr__(self):
+#         return '<Task %r>' % self.id
 
 
 # def :
@@ -30,7 +30,10 @@ def index():
 
 @app.route('/messagehelper/<string:title>')
 @app.route('/messagehelper/<string:title>/<string:content>')
-def message(title='Message Helper', content='On this page you can add your own title by typing in the addressbar after the <code>/title/&ltHere & gt</code>'):
+def message(
+    title='Message Helper', 
+    content='On this page you can add your own title by typing in the addressbar after the <code>/title/&ltHere & gt</code>'
+    ):
     # decent example of GET use
     return render_template('messagehelper.html', title=title, content=content)
 
@@ -42,7 +45,6 @@ def login():
         return redirect('/messagehelper/Logged In/You have successfully logged in.')
     else:
         return render_template('login.html', title="Login")
-
 
 if __name__ == "__main__":
     app.run(debug=True)
